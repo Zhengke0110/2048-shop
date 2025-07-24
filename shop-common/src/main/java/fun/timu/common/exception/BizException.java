163bca32c5e90feb4bf7c47d64089076ea8660ea
zhengke <zhengke996@163.com>
2025-07-24 10:20:24 +0800

    feat: 初始化 2048 商城项目
    - 创建项目结构和基础配置
    - 添加公共模块 shop-common
    - 实现基础异常处理和 JSON 数据封装
    -配置 Maven 依赖管理
    - 设置 Idea 项目配置

---
+++ shop-common/src/main/java/fun/timu/common/exception/BizException.java
@@ -0,0 +1,23 @@
+package fun.timu.common.exception;
+
+import fun.timu.common.enums.BizCodeEnum;
+import lombok.Data;
+
+@Data
+public class BizException extends RuntimeException {
+
+    private int code;
+    private String msg;
+
+    public BizException(int code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+}
